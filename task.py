dab7da25a156565f0f282cf0de1b25c0446c59e4
ahmed-mohammed32 <ahmedmoha3256@gmail.com>
2025-04-19 16:14:13 -0500

    Delete task.py

--- task.py
+++
@@ -1,9 +0,0 @@
-# Band Name Generator
-
-print("Welcome to the Band Name Generator.\n")
-
-# User inputs
-city_name = input("What is the name of the city you grew up in?\n")
-pet_name = input("What's your pet's name\n")
-
-print("Your band name could be " + city_name + " " + pet_name)
