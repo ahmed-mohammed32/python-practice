0c7c49630844c9debca3b6cf24a27466a5789d47
ahmedmoha3256 <ahmedmoha3256@gmail.com>
2025-05-17 17:09:23 -0500

    Day 16 - OOP

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,4 +1,35 @@
+# Object Oriented Programming - Coffee Maker
+# Report functionality is bugged
+
+import sys
+
+from Day_16.menu import MenuItem
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+is_on = True
+# Coffee maker report object
+report = CoffeeMaker()
+# Coffee maker resources object
+resources = CoffeeMaker()
+# Drinks object
+drink = Menu()
+# Payment object
+payment = MoneyMachine()
+
+cost = 0
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if choice == 'off':
+        sys.exit()
+    elif choice == 'report':
+        resources.report()
+        payment.report()
+    elif choice in ['espresso', 'latte', 'cappuccino']:
+        if resources.is_resource_sufficient(drink.find_drink(choice)):
+            payment.make_payment(cost)
+            report.make_coffee(drink.find_drink(choice))
--- Day_16/practice.py
+++ Day_16/practice.py
@@ -3,21 +3,21 @@ from another_module import another_variable
 from prettytable import PrettyTable
 print(another_variable)
 
-# # Create a new object from a Blueprint
-# timmy = Turtle()
-# print(timmy)
-#
-# # Call methods that are associated with that object
-# timmy.shape("turtle")
-# timmy.color("DarkGreen")
-# timmy.forward(100)
-#
-# # Create a new object from a Blueprint
-# my_screen = Screen()
-#
-# # Tap into it's attribute
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+# Create a new object from a Blueprint
+timmy = Turtle()
+print(timmy)
+
+# Call methods that are associated with that object
+timmy.shape("turtle")
+timmy.color("DarkGreen")
+timmy.forward(100)
+
+# Create a new object from a Blueprint
+my_screen = Screen()
+
+# Tap into it's attribute
+print(my_screen.canvheight)
+my_screen.exitonclick()
 
 # Create the table object
 table = PrettyTable()
