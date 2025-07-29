f8b009d535bd6be7ef8762072108919205c6fcbc
ahmedmoha3256 <ahmedmoha3256@gmail.com>
2025-07-28 23:42:45 -0500

    Adding practice fileOC

--- Day_18/part_4.py
+++ Day_18/part_4.py
@@ -1,1 +1,23 @@
+import turtle as t
+import random
 
+tim = t.Turtle()
+t.colormode(255)
+
+def random_color() -> tuple[int, int, int]:
+    '''Returns a random color within a tuple'''
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color_tuple = (r, g, b)
+    return color_tuple
+
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
