3ca6ef59abc2b9c8859681631d223c51c9d2b7f5
ahmedmoha3256 <ahmedmoha3256@gmail.com>
2025-07-30 22:41:39 -0500

    Practice

--- Day_18/part_4.py
+++ Day_18/part_4.py
@@ -1,3 +1,4 @@
+from turtle import Screen
 import turtle as t
 import random
 
@@ -12,12 +13,29 @@ def random_color() -> tuple[int, int, int]:
     color_tuple = (r, g, b)
     return color_tuple
 
+### Draws a random walk with tuples
+# directions = [0, 90, 180, 270]
+# tim.pensize(15)
+# tim.speed("fastest")
+#
+# for _ in range(200):
+#     tim.color(random_color())
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
+def draw_spirogrpah(radius):
+    '''Draws a spirograph based on the radius given'''
+    for _ in range(int(360 / radius)):
+        # New random color
+        tim.color(random_color())
+        # Draws a circle
+        tim.circle(100)
+        # Turns the tutle by the angle given
+        turtle_head = tim.heading()
+        tim.setheading(turtle_head + radius)
+
 tim.speed("fastest")
+draw_spirogrpah(5)
 
-for _ in range(200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+screen = Screen()
+screen.exitonclick()
